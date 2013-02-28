88f6e36e5aecc46270a1fb799e5dcb0d7e44160e
Agustin F. Calderon M <agustincl@gmail.com>
2013-02-28 11:34:20 +0100

    CRUD Gateway.

---
+++ CrudGateway/application/models/dataGatewayFiles.php
@@ -0,0 +1,28 @@
+<?php
+
+
+function readUsers()
+{
+
+	return $array|FALSE;
+}
+
+function readUser($id)
+{
+
+	return $array|FALSE;
+}
+
+
+function saveUser($id=NULL)
+{
+	
+	return $id|FALSE;
+}
+
+
+function deleteUser($id)
+{
+	
+	return TRUE|FALSE;
+}
